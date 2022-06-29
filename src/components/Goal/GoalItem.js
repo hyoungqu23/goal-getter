847424c765e7db4dc46bf82a317aad22138c9a9a
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-30 08:42:04 +0900

    [feat] GoalItem Component
    GoalItem 추가 및 스타일 설정, 클릭 시 삭제 이벤트 구현

---
+++ src/components/Goal/GoalItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GoalItem = ({ children, onDelete, id }) => {
+  const handleItemClick = () => {
+    onDelete(id);
+  };
+
+  return <StyledGoalItem onClick={handleItemClick}>{children}</StyledGoalItem>;
+};
+
+export default GoalItem;
+
+const StyledGoalItem = styled.li`
+  margin: 1rem 0;
+  background: #8b005d;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  color: white;
+  padding: 1rem 2rem;
+  cursor: pointer;
+`;
